8daf0fd154949ecf921d3c212be179958eee2667
tresvezesheber <hebireaper@gmail.com>
2024-02-03 17:29:24 -0300

    Its Alive

---
+++ src/main/java/br/dev/hebio/entranceexamregistrationapi/EntranceExamRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.hebio.entranceexamregistrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceExamRegistrationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntranceExamRegistrationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/hebio/entranceexamregistrationapi/EntranceExamRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.hebio.entranceexamregistrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntranceExamRegistrationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
