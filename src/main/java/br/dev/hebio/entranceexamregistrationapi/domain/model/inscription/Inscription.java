a3c802cff6c4befd67c06925aa5f399a713e279b
tresvezesheber <hebireaper@gmail.com>
2024-02-06 16:42:16 -0300

    fix: default constructor of Inscription class
    Add @NoArgsConstructor annotation in the class.

--- src/main/java/br/dev/hebio/entranceexamregistrationapi/domain/model/inscription/Inscription.java
+++ src/main/java/br/dev/hebio/entranceexamregistrationapi/domain/model/inscription/Inscription.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 public class Inscription {
 
     @Id
